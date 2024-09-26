47723f3ac563750d04347d588d7c251197486c5c
Arif Hussain <arifh9821@gmail.com>
2024-09-26 15:38:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let angle=0;
+const line= document.querySelector('line');
+
+setInterval(() => {
+  angle += 2; // Increment the angle by 2 degrees
+  line.style.transform = `rotate(${angle}deg)`; // Apply the rotation
+}, 20); // Repeat every 20 milliseconds
